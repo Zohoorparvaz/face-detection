aa7766c03b1cc7d2b04a3f19f906938effb57fb4
Alireza Zohoor Parvaz <zohoorparvaz@gmail.com>
2023-02-28 10:36:06 -0700

    Creating the file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import './App.css'
+import Navbar from "./components/navigationBar/NavigationBar"
+import Logo from "./components/logo/Logo"
+import Rank from "./components/rank/Rank"
+import SubmitForm from "./components/submitForm/SubmitForm"
+import ImageBox from "./components/imageBox/ImageBox"
+import SignIn from "./components/signIn/SignIn"
+import Register from "./components/Register/Register"
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <Logo />
+      <Rank />
+      <SubmitForm />
+      <ImageBox />
+      <SignIn />
+      <Register />
     </div>
   );
 }
