4fae1ddf319b6766301bba90d1ba4bfc57c741b4
Alireza Zohoor Parvaz <zohoorparvaz@gmail.com>
2023-02-28 14:13:22 -0700

    Rank component and style

--- src/components/rank/Rank.js
+++ src/components/rank/Rank.js
@@ -1,8 +1,14 @@
 import React from "react";
+import "./rank.css"
 
 const Rank = () => {
+  let submits = 0;
+  let rank = 1;
   return (
-    <div></div>
+    <div className="rankContainer">
+      <p className="submits">You have submitted {submits} photos so far</p>
+      <p className="rank">You are on rank number {rank}</p>
+    </div>
   )
 }
 export default Rank
