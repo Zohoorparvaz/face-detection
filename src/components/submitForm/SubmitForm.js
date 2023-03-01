f190f6aad80e8b315cf143fa327399704059d736
Alireza Zohoor Parvaz <zohoorparvaz@gmail.com>
2023-02-28 23:14:44 -0700

    worked on minor errors

--- src/components/submitForm/SubmitForm.js
+++ src/components/submitForm/SubmitForm.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const SubmitForm = () => {
   return (
-    <div class="submitFormContainer">
-      <div class="row1">
+    <div className="submitFormContainer">
+      <div className="row1">
         <p>Enter your image for face detect</p>
       </div>
-      <div class="row2">
+      <div className="row2">
         <input type="text" placeholder="Copy the URL" />
         <button type="submit">Find Face</button>
       </div>
