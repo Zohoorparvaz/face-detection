a8ad3aecaf2d9b719e99bebbebc66ff5ca676af0
Alireza Zohoor Parvaz <zohoorparvaz@gmail.com>
2023-02-28 19:13:33 -0700

    installed Particles package

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css'
 import Navbar from "./components/navigationBar/NavigationBar"
 import Logo from "./components/logo/Logo"
 import Rank from "./components/rank/Rank"
@@ -7,6 +6,8 @@ import ImageBox from "./components/imageBox/ImageBox"
 import SignIn from "./components/signIn/SignIn"
 import Register from "./components/Register/Register"
 import "./App.css"
+import ParticlesBg from 'particles-bg'
+
 
 function App() {
   return (
@@ -20,6 +21,9 @@ function App() {
       <ImageBox />
       <SignIn />
       <Register />
+      {/* Replace type with these to change BG 
+      "color" "ball" "lines" "thick" "circle" "cobweb" "polygon" "square" "tadpole" "fountain" "random" "custom"*/}
+      <ParticlesBg color="#ffffff" num={10} type="cobweb" bg={true} />
     </div>
   );
 }
