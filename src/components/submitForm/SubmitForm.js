54e23937c4819b785ecb974f64644db23aeb54b6
Alireza Zohoor Parvaz <zohoorparvaz@gmail.com>
2023-02-28 18:00:33 -0700

    SubmitForm view and style

--- src/components/submitForm/SubmitForm.js
+++ src/components/submitForm/SubmitForm.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./submitForm.css"
 
 const SubmitForm = () => {
-
   return (
-    <div></div>
+    <div class="submitFormContainer">
+      <div class="row1">
+        <p>Enter your image for face detect</p>
+      </div>
+      <div class="row2">
+        <input type="text" placeholder="Copy the URL" />
+        <button type="submit">Find Face</button>
+      </div>
+    </div>
   )
 }
 export default SubmitForm
