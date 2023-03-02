a4868ad9b05fc3eb56ac9b3aa97e61de66006fba
Alireza Zohoor Parvaz <zohoorparvaz@gmail.com>
2023-03-01 19:45:06 -0700

    Use top and left to move dettached absolute block

--- src/components/imageBox/ImageBox.js
+++ src/components/imageBox/ImageBox.js
@@ -3,9 +3,11 @@ import React from "react";
 const ImageBox = ({ imageURL, box }) => {
   return (
     <div className="imgContainer">
-      <img id="face" className="imgBox" src={imageURL} alt="The URL file will be shown here" />
-      <span className="bounding-box" style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}>
-      </span>
+      <div className="center">
+        <img id="face" className="imgBox" src={imageURL} alt="The URL file will be shown here" />
+        <span className="bounding-box" style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}>
+        </span>
+      </div>
     </div>
   )
 }
