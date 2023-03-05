f27b2108c617a3d66bf05841e6c923ad20cfb7a9
Alireza Zohoor Parvaz <zohoorparvaz@gmail.com>
2023-03-05 10:02:54 -0700

    Initializing routes for register and signin

--- backend/server.js
+++ backend/server.js
@@ -28,13 +28,29 @@ const database = {
 }
 
 app.get("/", (req, res) => {
-  res.json('Great! It works')
+  res.json(database.users)
 })
 
 app.post("/signin", (req, res) => {
   if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
     res.json(`Welcome ${database.users[0].name}`)
   } else { res.status(404).json('User not found! Try again') }
+})
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push(
+    {
+      id: "3",
+      name: name,
+      password: password,
+      email: email,
+      entries: 0,
+      joined: new Date(),
+    }
+  );
+  res.json(database.users[database.users.length - 1])
+
 })
 
 app.listen(PORT, () => {
