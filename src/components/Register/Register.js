f23908463eda828df98eb975bb1a69ab5a4aa454
Alireza Zohoor Parvaz <zohoorparvaz@gmail.com>
2023-03-06 00:00:16 -0700

    Added register headline to form

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,6 +3,7 @@ import React from "react";
 const Register = ({ login }) => {
   return (
     <div className="regContainer">
+      <h3>Register</h3>
       <label htmlFor="username">Username</label>
       <input type="text" id="username" name="username" minLength="4" />
       <label htmlFor="password">Password</label>
