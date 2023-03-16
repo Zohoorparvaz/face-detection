3ef8af87fb030312dac699e117bd142193bc18cd
Alireza Zohoor Parvaz <zohoorparvaz@gmail.com>
2023-03-15 19:35:36 -0600

    update rank and entries after signin

--- src/components/rank/Rank.js
+++ src/components/rank/Rank.js
@@ -1,10 +1,9 @@
 import React from "react";
 
-const Rank = ({ user }) => {
-  let rank = 1;
+const Rank = ({ entries, rank }) => {
   return (
     <div className="rankContainer">
-      <p className="submits">You have submitted {user.entries} photos so far</p>
+      <p className="submits">You have submitted {entries} photos so far</p>
       <p className="rank">You are on rank number {rank}</p>
     </div>
   )
